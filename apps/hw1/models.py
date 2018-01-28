b8dea88e879884f638e082783a58ba633fd3f161
Aaron Arredondo Sanchez <binauralvoice@gmail.com>
2018-01-27 21:52:17 -0600

    first commit

---
+++ apps/hw1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.forms import ModelForm
+
+
+class Input(models.Model):
+    r = models.FloatField()
+
+class InputForm(ModelForm):
+    class Meta:
+        model = Input
+        fields = ('r',)
+        
